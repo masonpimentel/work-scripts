3627dd6660fe8ed2d50f19ab06b53eacf1ed1517
Mason Pimentel <mason.pimentel@glancepay.com>
2020-02-25 11:15:46 -0800

    Add offline

--- setup.sh
+++ setup.sh
@@ -3,4 +3,4 @@
 
 echo "1. Terminal initialization file? (pre-Catalina Mac this was /Users/<username>/.bash_profile, after it became /Users/<username>/.zshrc)"
 read termInit
-cat scriptsMaintenance/recPath | tail -n +3 > $termInit
+cat scriptsMaintenance/recPath | tail -n +3 >> $termInit
