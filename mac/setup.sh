69300436efb09233057f5a68a912f5bcfb98ad57
Mason Pimentel <mason.pimentel@xealth.io>
2022-02-20 11:38:55 -0800

    Add username to Google Drive config

--- mac/setup.sh
+++ mac/setup.sh
@@ -1,3 +1,6 @@
 echo "1. Terminal initialization file? (pre-Catalina Mac this was /Users/<username>/.bash_profile, after it became /Users/<username>/.zshrc)"
 read termInit
 cat scriptsMaintenance/recPath | tail -n +3 >> $termInit
+echo "2. Mac username (path of user root, ex. /Users/mason.pimentel - enter mason.pimentel)"
+read username
+sed -i '' "s/pathgoeshere/$username/" workflow/l
