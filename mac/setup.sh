b95262d6034a0d83e5b803efeecc76d1b24c99fd
Mason Pimentel <mason.pimentel@xealth.io>
2022-02-20 19:51:28 -0800

    Add -p to cp

--- mac/setup.sh
+++ mac/setup.sh
@@ -1,4 +1,4 @@
-cp -rf ../ ~/Scripts
+cp -rfp ../ ~/Scripts
 echo "1. Terminal initialization file? (pre-Catalina Mac this was /Users/<username>/.bash_profile, after it became /Users/<username>/.zshrc)"
 read termInit
 cat scriptsMaintenance/recPath | tail -n +3 > $termInit
